00aa6681995836a5476f5f5a0cbfba28fdfa3fd4
luis <luisfelipemourapereira@protonmail.com>
2023-12-01 10:45:08 +0100

    Gemfile update

--- lib/terraform-synthesizer.rb
+++ lib/terraform-synthesizer.rb
@@ -2,11 +2,21 @@ require %(abstract-synthesizer)
 
 class TerraformSynthesizer < AbstractSynthesizer
   def method_missing(method_name, ...)
+    if @in_locals
+      if args[0].nil?
+        raise ArgumentError,
+              %(not assigning anything to this local #{method_name})
+      end
+
+      @translation[:template][:locals][method_name.to_sym] = args[0]
+      @in_locals = false
+    end
     if method_name.to_s.eql?(%(locals))
       @translation = {} if @translation.nil?
       @translation[:template] = {} if @translation[:template].nil?
       @translation[:template][:locals] = {} if @translation[:template][:locals].nil?
       @translation[:template][:locals].merge!(yield) if block_given?
+      @in_locals = true
     else
       abstract_method_missing(
         method_name,
