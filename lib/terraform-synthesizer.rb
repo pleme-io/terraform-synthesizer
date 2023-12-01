049c90c3e565cc6d5b8f0081b10a47f3999e9f46
luis <luisfelipemourapereira@protonmail.com>
2023-12-01 11:17:54 +0100

    Gemfile update

--- lib/terraform-synthesizer.rb
+++ lib/terraform-synthesizer.rb
@@ -1,35 +1,38 @@
 require %(abstract-synthesizer)
 
 class TerraformSynthesizer < AbstractSynthesizer
-  def method_missing(method_name, ...)
+  KEYS = %i[
+    terraform
+    resource
+    variable
+    output
+    data
+  ].freeze
+
+  def method_missing(method_name, *args, &)
     if @in_locals
       if args[0].nil?
         raise ArgumentError,
               %(not assigning anything to this local #{method_name})
       end
 
-      @translation[:template][:locals][method_name.to_sym] = args[0]
       @in_locals = false
-    end
-    if method_name.to_s.eql?(%(locals))
+      @translation[:template][:locals].merge!(
+        { method_name.to_sym => args[0] }
+      )
+    elsif method_name.to_s.eql?(%(locals))
+
       @translation = {} if @translation.nil?
       @translation[:template] = {} if @translation[:template].nil?
       @translation[:template][:locals] = {} if @translation[:template][:locals].nil?
-      @translation[:template][:locals].merge!(yield) if block_given?
       @in_locals = true
+      yield
     else
-      return {} if @in_locals
-
       abstract_method_missing(
-        method_name,
-        %i[
-          terraform
-          resource
-          variable
-          output
-          data
-        ],
-        ...
+        method_name.to_sym,
+        KEYS,
+        *args,
+        &
       )
     end
   end
---
+++ spec/terraform_spec.rb
@@ -0,0 +1,45 @@
+require %(terraform-synthesizer)
+
+describe TerraformSynthesizer do
+  context %(main) do
+    let(:synth) do
+      described_class.new
+    end
+
+    it %(should compile small declaration and be hash) do
+      synth.synthesize do
+        resource :aws_vpc, :thing do
+          cidr_block %(10.0.0.0/16)
+        end
+        locals do
+          special_var %(special_value)
+        end
+      end
+      expect(synth.synthesis).to be_kind_of(Hash)
+    end
+
+    it %(should contain locals) do
+      synth.synthesize do
+        resource :aws_vpc, :thing do
+          cidr_block %(10.0.0.0/16)
+        end
+        locals do
+          special_var %(special_value)
+        end
+      end
+      expect(synth.synthesis[:locals][:special_var]).to be_kind_of(String)
+    end
+
+    it %(should contain resources) do
+      synth.synthesize do
+        resource :aws_vpc, :thing do
+          cidr_block %(10.0.0.0/16)
+        end
+        locals do
+          special_var %(special_value)
+        end
+      end
+      expect(synth.synthesis[:resource][:aws_vpc][:thing]).to be_kind_of(Hash)
+    end
+  end
+end
