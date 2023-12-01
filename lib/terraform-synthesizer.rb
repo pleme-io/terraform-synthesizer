86006acc329b998bd7a25d3b7cd9c39e4f6664a8
luis <luisfelipemourapereira@protonmail.com>
2023-12-01 13:19:41 +0100

    Gemfile update

--- lib/terraform-synthesizer.rb
+++ lib/terraform-synthesizer.rb
@@ -9,6 +9,18 @@ class TerraformSynthesizer < AbstractSynthesizer
     data
   ].freeze
 
+  ##############################################################################
+  # notes:
+  #
+  # locals are processed as a direct block like
+  # locals do
+  #   key value
+  # end
+  # while resources are processed as resource like blocks
+  # resource :aws_vpc, :virtual_name do
+  #   key value
+  # end
+  ##############################################################################
   def method_missing(method_name, *args, &)
     if @in_locals
       if args[0].nil?
@@ -16,13 +28,16 @@ class TerraformSynthesizer < AbstractSynthesizer
               %(not assigning anything to this local #{method_name})
       end
 
-      @in_locals = false
-      @translation[:template][:locals][method_name.to_sym] = args[0]
+      @in_locals                                            = false
+      @translation[:template][:locals][method_name.to_sym]  = args[0]
+
     elsif method_name.to_s.eql?(%(locals))
-      @translation = {} if @translation.nil?
-      @translation[:template] = {} if @translation[:template].nil?
-      @translation[:template][:locals] = {} if @translation[:template][:locals].nil?
-      @in_locals = true
+
+      @translation                      = {} if @translation.nil?
+      @translation[:template]           = {} if @translation[:template].nil?
+      @translation[:template][:locals]  = {} if @translation[:template][:locals].nil?
+      @in_locals                        = true
+
       yield
     else
       abstract_method_missing(
