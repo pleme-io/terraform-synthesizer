4daa8df1f2af76a727ed2796c2a0d69553980022
luis <luisfelipemourapereira@protonmail.com>
2023-12-01 13:30:27 +0100

    Gemfile update

--- lib/terraform-synthesizer.rb
+++ lib/terraform-synthesizer.rb
@@ -54,30 +54,5 @@ class TerraformSynthesizer < AbstractSynthesizer
         )
       end
     end
-    # if @in_locals
-    #   if args[0].nil?
-    #     raise ArgumentError,
-    #           %(not assigning anything to this local #{method_name})
-    #   end
-    #
-    #   @in_locals                                            = false
-    #   @translation[:template][:locals][method_name.to_sym]  = args[0]
-    #
-    # elsif method_name.to_s.eql?(%(locals))
-    #
-    #   @translation                      = {} if @translation.nil?
-    #   @translation[:template]           = {} if @translation[:template].nil?
-    #   @translation[:template][:locals]  = {} if @translation[:template][:locals].nil?
-    #   @in_locals                        = true
-    #
-    #   yield
-    # else
-    #   abstract_method_missing(
-    #     method_name.to_sym,
-    #     RESOURCE_KEYS,
-    #     *args,
-    #     &
-    #   )
-    # end
   end
 end
