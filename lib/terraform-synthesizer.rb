ef67b9c283a48b2c821f4fa21f8925e3a10b6992
luis <luisfelipemourapereira@protonmail.com>
2023-12-01 10:29:02 +0100

    Gemfile update

--- lib/terraform-synthesizer.rb
+++ lib/terraform-synthesizer.rb
@@ -1,14 +1,14 @@
 require %(abstract-synthesizer)
 
 class TerraformSynthesizer < AbstractSynthesizer
-  def method_missing(method_name, *args, &block)
+  def method_missing(method_name, *args, &)
     if method_name.to_s.eql?(%(locals))
       keys = args[0]
       if keys.length.to_s.eql(%(0))
         @translation = {} if @translation.nil?
         @translation[:template] = {} if @translation[:template].nil?
         @translation[:template][:locals] = {} if @translation[:template][:locals].nil?
-        @translation[:template][:locals].merge!(block.call) if block_given?
+        @translation[:template][:locals].merge!(instance_eval(&)) if block_given?
       else
         raise ArgumentError, %(key length for locals was more than 0, this is bad)
       end
@@ -25,7 +25,7 @@ class TerraformSynthesizer < AbstractSynthesizer
         data
       ],
       *args,
-      &block
+      &
     )
   end
 end
